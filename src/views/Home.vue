bc43dbcf5f7887e46fc5f421b91ba3d38635e6a2
feifeigd <feifeigd@21cn.com>
2019-09-19 16:00:03 +0800

    添加属性和方法

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png" @click="onClick">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
@@ -12,8 +12,15 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 // @Component 修饰符注明了此类为一个 Vue 组件
 @Component({
   components: {
-    HelloWorld,
+    HelloWorld, // 引用到的组件
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  // 初始数据可以直接声明为实例的属性
+  private message = 'Hello!';
+  // 组件方法也可以直接声明为实例的方法
+  private onClick() {
+    window.alert(this.message);
+  }
+}
 </script>
