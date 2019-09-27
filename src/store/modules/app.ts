611e46ce615bec3fd3b3922ec12c1a51e12e7e3e
feifeigd <feifeigd@21cn.com>
2019-09-27 21:20:46 +0800

    app模块

--- src/store/modules/app.ts
+++ src/store/modules/app.ts
@@ -1,5 +1,10 @@
-import { VuexModule, getModule } from 'vuex-module-decorators';
-import { getSidebarStatus } from '@/utils/cookies';
+import { VuexModule, getModule, Module, Mutation, Action } from 'vuex-module-decorators';
+import { getSidebarStatus, getSize, setSidebarStatus, setLanguage, setSize } from '@/utils/cookies';
+import { getLocale } from '@/lang';
+
+import store from '@/store';
+import { TimeSelect } from 'element-ui';
+
 export enum DeviceType {
     Mobile,
     Desktop,
@@ -7,17 +12,78 @@ export enum DeviceType {
 
 export interface IAppState {
     device: DeviceType,
+    language: string,
     sidebar: {
         opened: boolean,
         withoutAnimation: boolean,
-    }
+    },
+    size: string,
 }
 
+@Module({ dynamic: true, store, name: 'app' })
 class App extends VuexModule implements IAppState {
     public device = DeviceType.Desktop;
+    public language = getLocale();
     public sidebar = {
         opened: getSidebarStatus() !== 'closed',
         withoutAnimation: false,
     };
+    public size = getSize() || 'medium';
+
+    @Mutation
+    private TOGGLE_SIDEBAR(withoutAnimation: boolean){
+        this.sidebar.opened = !this.sidebar.opened;
+        this.sidebar.withoutAnimation = withoutAnimation;
+        setSidebarStatus (this.sidebar.opened ? 'opened' : 'closed');
+    }
+
+    @Mutation
+    private CLOSE_SIDEBAR(withoutAnimation: boolean){
+        this.sidebar.opened = false;
+        this.sidebar.withoutAnimation = withoutAnimation;
+        setSidebarStatus('closed');
+    }
+
+    @Mutation
+    private TOGGLE_DEVICE(device: DeviceType){
+        this.device = device;
+    }
+
+    @Mutation
+    private SET_LANGUAGE(language: string){
+        this.language = language;
+        setLanguage(language);
+    }
+
+    @Mutation
+    private SET_SIZE(size: string){
+        this.size = size;
+        setSize(size);
+    }
+
+    @Action
+    public ToggleSideBar(withoutAnimation: boolean){
+        this.TOGGLE_SIDEBAR(withoutAnimation);
+    }
+
+    @Action
+    public CloseSideBar(withoutAnimation: boolean){
+        this.CLOSE_SIDEBAR(withoutAnimation);
+    }
+
+    @Action
+    public ToggleDevice(device: DeviceType){
+        this.TOGGLE_DEVICE(device);
+    }
+
+    @Action
+    public SetLanguage(language: string){
+        this.SET_LANGUAGE(language);
+    }
+
+    @Action
+    public SetSize(size: string){
+        this.SET_SIZE(size);
+    }
 }
 export const AppModule = getModule(App);
