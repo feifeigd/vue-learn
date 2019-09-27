5bbd1b1387179a7d37ff920b0bf5da13afe60b31
feifeigd <feifeigd@21cn.com>
2019-09-27 20:12:49 +0800

    cookies

--- src/store/modules/app.ts
+++ src/store/modules/app.ts
@@ -1,5 +1,5 @@
 import { VuexModule, getModule } from 'vuex-module-decorators';
-
+import { getSidebarStatus } from '@/utils/cookies';
 export enum DeviceType {
     Mobile,
     Desktop,
@@ -7,9 +7,17 @@ export enum DeviceType {
 
 export interface IAppState {
     device: DeviceType,
+    sidebar: {
+        opened: boolean,
+        withoutAnimation: boolean,
+    }
 }
 
 class App extends VuexModule implements IAppState {
     public device = DeviceType.Desktop;
+    public sidebar = {
+        opened: getSidebarStatus() !== 'closed',
+        withoutAnimation: false,
+    };
 }
 export const AppModule = getModule(App);
--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -7,3 +7,14 @@ export const setSidebarStatus = (sidebarStatus: string) => Cookies.set(sidebarSt
 
 const languageKey = 'language';
 export const getLanguage = () => Cookies.get(languageKey);
+export const setLanguage = (language: string) => Cookies.set(languageKey, language);
+
+const sizeKey = 'size';
+export const getSize = () => Cookies.get(sizeKey);
+export const setSize = (size: string) => Cookies.set(sizeKey, size);
+
+// User
+const tokenKey = 'vue_typescript_admin_access_token';
+export const getToken = () => Cookies.get(tokenKey);
+export const setToken = (token: string) => Cookies.set(tokenKey, token);
+export const removeToken = () => Cookies.remove(tokenKey);
