3a4e905295e53232cb9e0232a0c9c806caf10df4
feifeigd <feifeigd@21cn.com>
2019-09-28 21:58:30 +0800

    SidebarItemLink

---
+++ src/layout/components/Sidebar/SidebarItemLink.vue
@@ -0,0 +1,24 @@
+<template>
+    <!-- 外部链接 -->
+    <a v-if="isExternam(to)" :href="to" target="_blank" rel="noopener">
+        <slot/>
+    </a>
+    <router-link v-else :to="to">
+        <slot/>
+    </router-link>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Mixins } from 'vue-property-decorator';
+import { isExternal } from '@/utils/validate';
+
+@Component({
+    name: 'SidebarItemLink',
+})
+export default class SidebarItemLink extends Vue {
+    @Prop({ required: true })
+    private to!: string;
+
+    private isExternal = isExternal;
+}
+</script>
