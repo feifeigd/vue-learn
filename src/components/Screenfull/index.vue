b6876020b6b2ab5d6bc837dbc44d74dfc0004bad
feifeigd <feifeigd@21cn.com>
2019-09-30 20:07:29 +0800

    全屏

---
+++ src/components/Screenfull/index.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="screenfull">
+        <svg-icon :name="isFullscreen? 'exit-fullscreen' : 'fullscreen'" @click="click"/>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import screenfull from 'screenfull';
+
+const sf = screenfull;
+@Component
+export default class Screenfull extends Vue {
+    private isFullscreen = false;
+
+    mounted(){
+        if (sf && sf.enabled){
+            sf.on('change', this.change);
+        }
+    }
+
+    beforeDestory(){
+        if (sf && sf.enabled){
+            sf.off('change', this.change);
+        }
+    }
+
+    private change(){
+        if (sf && sf.enabled){
+            this.isFullscreen = sf.isFullscreen;
+        }
+    }
+
+    private click(){
+        if (sf){
+            if(!sf.enabled){
+                this.$message({
+                    message: 'you browser cannot work',
+                    type: 'warning',
+                });
+                return false;
+            }
+            sf.toggle();
+        }
+    }
+}
+</script>
